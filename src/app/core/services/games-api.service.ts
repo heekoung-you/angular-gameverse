8e24da9bc3a48f378bd6739858260312317fff17
heekoung-you <151024763+heekoung-you@users.noreply.github.com>
2025-10-15 13:23:33 +0200

    Load more on scrollable api called. error handle - show retry button loading indicator to show loading options

--- src/app/core/services/games-api.service.ts
+++ src/app/core/services/games-api.service.ts
@@ -7,7 +7,7 @@ import {
   GamesScreenshotsList200Response,
   GamesService,
 } from '../../api-client';
-import { catchError, map, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of, throwError } from 'rxjs';
 import { HttpResponse } from '@angular/common/http';
 
 @Injectable({
@@ -15,13 +15,26 @@ import { HttpResponse } from '@angular/common/http';
 })
 export class GamesApiService {
   constructor(private api: GamesService) {}
-  getGames(): Observable<Game[]> {
-    // TODO get from params
+  getGames({
+    pageNumber = 1,
+    pageSize = 5,
+    tag,
+  }: {
+    pageNumber?: number;
+    pageSize?: number;
+    tag?: string | undefined;
+  }): Observable<Game[]> {
+    // Simulate 30% chance of error
+    const shouldFail = Math.random() < 0.3;
+    if (shouldFail) {
+      return throwError(() => new Error('Simulated random error'));
+    }
+
     // TODO put behaviorSubject for saving error.
     return this.api
       .gamesList(
-        1, // page
-        20 // pageSize
+        pageNumber, // page
+        pageSize // pageSize
       )
       .pipe(
         map((res: GamesList200Response) => res.results),
--- src/app/pages/games/games.component.ts
+++ src/app/pages/games/games.component.ts
@@ -1,9 +1,17 @@
-import { Component, DestroyRef, input, OnInit, signal } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  DestroyRef,
+  ElementRef,
+  input,
+  OnInit,
+  signal,
+  viewChild,
+} from '@angular/core';
 import { GamesApiService } from '../../core/services/games-api.service';
 import { Game } from '../../api-client';
 import { GameCardComponent } from '../../components/game-card/game-card.component';
 import { HeaderTextComponent } from '../../components/header-text/header-text.component';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-games',
@@ -11,27 +19,88 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './games.component.html',
   styleUrl: './games.component.scss',
 })
-export class GamesComponent implements OnInit {
+export class GamesComponent implements OnInit, AfterViewInit {
   title = input<string>();
   description = input<string>();
   promoText = input<string>();
 
+  pageNumber = signal<number>(1);
+  isLoading = signal<boolean>(false);
+  hasError = signal<boolean>(false);
+  games = signal<Game[]>([]);
+
+  // TODO : Check move constructor to inject
+  // Check if base component to adding - DestroyRef and inherits for this? or just inject (Maybe later create LoggerService to log errors/info)
   constructor(private gamesApiService: GamesApiService, private destroyRef: DestroyRef) {}
 
+  // scrolltrigger template reference variable
+  scrollTrigger = viewChild.required<ElementRef<HTMLDivElement>>('scrollTrigger');
+
+  private observer!: IntersectionObserver;
   ngOnInit(): void {
     this.loadGames();
   }
 
-  games = signal<Game[]>([]);
+  ngAfterViewInit(): void {
+    this.setupIntersectionObserver();
+  }
 
-  loadGames() {
-    const games$ = this.gamesApiService.getGames();
-    const sub = games$.subscribe((games) => {
-      this.games.set(games);
+  loadGames(hardReload: boolean = false) {
+    if (hardReload) {
+      this.pageNumber.set(1);
+      this.games.set([]);
+    }
+
+    const games$ = this.gamesApiService.getGames({ pageNumber: this.pageNumber() });
+    this.isLoading.set(true);
+    this.hasError.set(false);
+    const nextPage = this.pageNumber() + 1;
+
+    const sub = games$.subscribe({
+      next: (games) => {
+        this.games.update((value) => [...value, ...games]);
+        this.pageNumber.set(nextPage);
+        this.isLoading.set(false);
+      },
+      error: (error) => {
+        //console.error('Error loading games:', error);
+        //this.games.set([]);
+        console.log(
+          'Error loading games: pageNumber:',
+          this.pageNumber(),
+          ',games:',
+          this.games().length
+        );
+        this.hasError.set(true);
+        this.isLoading.set(false);
+      },
     });
 
     this.destroyRef.onDestroy(() => {
       sub.unsubscribe();
     });
+  }
+
+  private setupIntersectionObserver() {
+    const options = {
+      root: null, //document.querySelector('#game-list'),
+      rootMargin: '0px',
+      scrollMargin: '0px',
+      threshold: 1.0,
+    };
+
+    this.observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && !this.isLoading() && !this.hasError()) {
+        //this.pageNumber.set(this.pageNumber() + 1);
+        console.log('Loading more games, page:', this.pageNumber());
+
+        // Adding delay to simulate loading time and allow isLoading state to be visible
+        setTimeout(() => {
+          this.loadGames(false);
+        }, 500);
+      }
+    }, options);
+
+    this.observer.observe(this.scrollTrigger().nativeElement);
   }
 }
