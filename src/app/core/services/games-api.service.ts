0e84cf48a198fadaf5a507bc76053279511cd0b8
heekoung-you <151024763+heekoung-you@users.noreply.github.com>
2025-10-16 12:25:56 +0200

    Games Lists page extended with tag viewer plus test extended

--- src/app/core/services/games-api.service.ts
+++ src/app/core/services/games-api.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { inject, Inject, Injectable } from '@angular/core';
 import {
   ConfigurationParameters,
   Game,
@@ -6,6 +6,10 @@ import {
   GamesList200Response,
   GamesScreenshotsList200Response,
   GamesService,
+  Genre,
+  GenresService,
+  Tag,
+  TagsService,
 } from '../../api-client';
 import { catchError, map, Observable, of, throwError } from 'rxjs';
 import { HttpResponse } from '@angular/common/http';
@@ -14,27 +18,40 @@ import { HttpResponse } from '@angular/common/http';
   providedIn: 'root',
 })
 export class GamesApiService {
-  constructor(private api: GamesService) {}
+  gamesApi = inject(GamesService);
+  tagsApi = inject(TagsService);
+  genresApi = inject(GenresService);
+
+  //constructor(private api: GamesService) {}
   getGames({
     pageNumber = 1,
     pageSize = 5,
-    tag,
+    genre,
   }: {
     pageNumber?: number;
     pageSize?: number;
-    tag?: string | undefined;
+    genre?: string | undefined;
   }): Observable<Game[]> {
     // Simulate 30% chance of error
-    const shouldFail = Math.random() < 0.3;
-    if (shouldFail) {
-      return throwError(() => new Error('Simulated random error'));
-    }
+    // const shouldFail = Math.random() < 0.3;
+    // if (shouldFail) {
+    //   return throwError(() => new Error('Simulated random error'));
+    // }
 
     // TODO put behaviorSubject for saving error.
-    return this.api
+    return this.gamesApi
       .gamesList(
         pageNumber, // page
-        pageSize // pageSize
+        pageSize, // pageSize
+        undefined, // search
+        undefined, // searchPrecise
+        undefined, // searchExact
+        undefined, // parentPlatforms
+        undefined, // platforms
+        undefined, // stores
+        undefined, // developers
+        undefined, // publishers
+        genre // genres
       )
       .pipe(
         map((res: GamesList200Response) => res.results),
@@ -43,10 +60,17 @@ export class GamesApiService {
   }
 
   getGameDetail(id: string): Observable<GameSingle> {
-    return this.api.gamesRead(id);
+    return this.gamesApi.gamesRead(id);
   }
 
   getScreenshots(id: string): Observable<GamesScreenshotsList200Response> {
-    return this.api.gamesScreenshotsList(id);
+    return this.gamesApi.gamesScreenshotsList(id);
+  }
+
+  getGenres(): Observable<Genre[]> {
+    return this.genresApi.genresList(undefined, 1, 20).pipe(
+      map((res) => res.results?.map((genre) => genre) ?? []),
+      catchError(() => of([]))
+    );
   }
 }
--- src/app/pages/games/games.component.ts
+++ src/app/pages/games/games.component.ts
@@ -2,14 +2,16 @@ import {
   AfterViewInit,
   Component,
   DestroyRef,
+  effect,
   ElementRef,
+  inject,
   input,
   OnInit,
   signal,
   viewChild,
 } from '@angular/core';
 import { GamesApiService } from '../../core/services/games-api.service';
-import { Game } from '../../api-client';
+import { Game, Genre } from '../../api-client';
 import { GameCardComponent } from '../../components/game-card/game-card.component';
 import { HeaderTextComponent } from '../../components/header-text/header-text.component';
 
@@ -20,47 +22,81 @@ import { HeaderTextComponent } from '../../components/header-text/header-text.co
   styleUrl: './games.component.scss',
 })
 export class GamesComponent implements OnInit, AfterViewInit {
+  private gamesApiService = inject(GamesApiService);
+  private destroyRef = inject(DestroyRef);
+  private observer!: IntersectionObserver;
+
+  // page input for normal title/description/promo text
   title = input<string>();
   description = input<string>();
   promoText = input<string>();
 
+  // signals values for page state management
   pageNumber = signal<number>(1);
   isLoading = signal<boolean>(false);
   hasError = signal<boolean>(false);
   games = signal<Game[]>([]);
+  genres = signal<Genre[]>([]);
+  selectedGenre = signal<string | undefined>(undefined);
+  isGenresExpanded = signal<boolean>(false);
 
-  // TODO : Check move constructor to inject
-  // Check if base component to adding - DestroyRef and inherits for this? or just inject (Maybe later create LoggerService to log errors/info)
-  constructor(private gamesApiService: GamesApiService, private destroyRef: DestroyRef) {}
-
-  // scrolltrigger template reference variable
+  // scrolltrigger template reference variable - for infinite scroll load more games
   scrollTrigger = viewChild.required<ElementRef<HTMLDivElement>>('scrollTrigger');
 
-  private observer!: IntersectionObserver;
+  // genre container to find out if any overflow tags there
+  genreContainer = viewChild.required<ElementRef<HTMLDivElement>>('genreContainer');
+  hasGenreOverflow = signal(false);
+
   ngOnInit(): void {
-    this.loadGames();
+    this.loadGames(undefined, true);
+    this.loadGenres();
   }
 
   ngAfterViewInit(): void {
     this.setupIntersectionObserver();
+
+    this.setupGenreOverflowCheck();
   }
 
-  loadGames(hardReload: boolean = false) {
+  loadGames(genre: string | undefined, hardReload: boolean = false) {
     if (hardReload) {
       this.pageNumber.set(1);
-      this.games.set([]);
+      //this.games.set([]);
     }
 
-    const games$ = this.gamesApiService.getGames({ pageNumber: this.pageNumber() });
+    const games$ = this.gamesApiService.getGames({
+      pageNumber: this.pageNumber(),
+      pageSize: 5,
+      genre,
+    });
+
     this.isLoading.set(true);
     this.hasError.set(false);
     const nextPage = this.pageNumber() + 1;
 
     const sub = games$.subscribe({
       next: (games) => {
-        this.games.update((value) => [...value, ...games]);
+        this.games.update((value) => {
+          if (hardReload) {
+            return [...games];
+          } else {
+            return [...value, ...games];
+          }
+        });
         this.pageNumber.set(nextPage);
         this.isLoading.set(false);
+        console.log(
+          'loadGames called, genre:',
+          genre,
+          ', hardReload:',
+          hardReload,
+          ', pageNumber:',
+          this.pageNumber(),
+          ', games loaded:',
+          games.length,
+          'Game names',
+          games.map((g) => g.name).join(', ')
+        );
       },
       error: (error) => {
         //console.error('Error loading games:', error);
@@ -81,7 +117,37 @@ export class GamesComponent implements OnInit, AfterViewInit {
     });
   }
 
+  loadGenres() {
+    const genres$ = this.gamesApiService.getGenres();
+    const sub = genres$.subscribe({
+      next: (genres) => {
+        console.log('Loaded genres:', genres);
+        this.genres.set(genres);
+      },
+      error: (error) => {
+        console.error('Error loading genres:', error);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      sub.unsubscribe();
+    });
+  }
+
+  onGenreSelect(genre: Genre) {
+    if (genre.slug === this.selectedGenre()) {
+      // Deselect if the same genre is clicked
+      this.selectedGenre.set(undefined);
+    } else {
+      if (genre.slug) {
+        this.selectedGenre.set(genre.slug);
+      }
+    }
+    this.loadGames(this.selectedGenre(), true);
+  }
+
   private setupIntersectionObserver() {
+    // TODO : need to learn more about this section
     const options = {
       root: null, //document.querySelector('#game-list'),
       rootMargin: '0px',
@@ -90,17 +156,46 @@ export class GamesComponent implements OnInit, AfterViewInit {
     };
 
     this.observer = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting && !this.isLoading() && !this.hasError()) {
-        //this.pageNumber.set(this.pageNumber() + 1);
+      if (
+        entries[0].isIntersecting &&
+        !this.isLoading() &&
+        !this.hasError() &&
+        this.pageNumber() > 1
+      ) {
+        //this.pageNumber.set(this.pageNumber() + 1); <- move this logic after successful called from load games - more reliable if error happens.
         console.log('Loading more games, page:', this.pageNumber());
 
         // Adding delay to simulate loading time and allow isLoading state to be visible
         setTimeout(() => {
-          this.loadGames(false);
-        }, 500);
+          this.loadGames(this.selectedGenre(), false);
+        }, 100);
       }
     }, options);
 
     this.observer.observe(this.scrollTrigger().nativeElement);
+  }
+
+  private setupGenreOverflowCheck() {
+    const container = this.genreContainer().nativeElement;
+    const checkOverflow = () => {
+      const firstButton = container.querySelector('button');
+      if (!firstButton) return;
+      const oneLineHeight = firstButton.clientHeight;
+      const totalHeight = container.scrollHeight;
+      this.hasGenreOverflow.set(totalHeight > oneLineHeight * 1.5);
+    };
+
+    const observer = new ResizeObserver(checkOverflow);
+    observer.observe(container);
+
+    queueMicrotask(checkOverflow);
+
+    this.destroyRef.onDestroy(() => {
+      observer.disconnect();
+    });
+  }
+
+  toggleGenres() {
+    this.isGenresExpanded.set(!this.isGenresExpanded());
   }
 }
