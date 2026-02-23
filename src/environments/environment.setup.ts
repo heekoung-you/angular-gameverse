57317f8a190df2ed6ec45fa45f515751180a9b33
heekoung-you <heekoung.you@ibm.com>
2026-02-23 08:52:39 +0100

    Remove environment file and ignore it

---
+++ src/environments/environment.setup.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  rawgApiKey: 'API-KEY',
+  rawgApiUrl: '',
+
+  firebaseConfig: {
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
+    measurementId: '',
+  },
+};
